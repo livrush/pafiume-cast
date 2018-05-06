2b3dd0c2cc1d2f727b02351879afc42632e674f9
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-06 16:59:54 -0500

    add first component for podcast icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './components/PodcastIcon.js';
+import PodcastIcon from './components/PodcastIcon.js';
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <PodcastIcon />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/PodcastIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PodcastIcon(props) {
+  return (
+    <div className="podcast-icon">
+      <img src="https://picsum.photos/200/200?image=0" />
+    </div>
+  )
+}
+
+export default PodcastIcon;
