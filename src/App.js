ad1da7f922516c2e6ac3e576c60abb0384c74229
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-07 20:16:43 -0500

    reimplement app to read from rss feeds instead of apple api

--- src/App.js
+++ src/App.js
@@ -24,29 +24,46 @@ class App extends Component {
     let parser = new Parser();
     const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
 
-    parser.parseURL(CORS_PROXY + 'https://www.npr.org/rss/podcast.php?id=510308')
-      .then(console.log);
+    // parser.parseURL(CORS_PROXY + )
+    //   .then(console.log);
 
     const component = this;
     const appleAPI = 'https://itunes.apple.com/search?entity=podcast&attribute=titleTerm&term='
-    axios('/podcasts.txt')
+    axios('/rss-feeds.txt')
       .then(({ data }) => data.split('\n'))
-      .then(podcastNames => {
-        const podcastCalls = podcastNames.map((podcastName) => axios(`${appleAPI}${podcastName.split(' ').join('+')}`));
-        // axios('https://itunes.apple.com/search?entity=podcast&term=npr')
-        Promise.all(podcastCalls)
-          .then((responses) => responses.reduce((acc, { data }) => acc.concat(data.results), []))
-          .then(podcasts => podcasts.sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate)))
-          .then((podcasts) => component.setState({ podcasts }))
-          .catch(console.error);
+      .then(rssFeedUrls => {
+        const rssFeedRequests = rssFeedUrls.map((feedUrl) => parser.parseURL(CORS_PROXY + feedUrl));
+        Promise.all(rssFeedRequests)
+          .then(responses => responses.map(response => { response.items = response.items.slice(0, 5); return response; }))
+          .then(responses => responses.map(response => { response.items = response.items.slice(0, 5); return response; }))
+          .then(responses => responses.reduce((acc, res) => acc.concat(res.items), []))
+          .then(podcasts => podcasts.sort((a, b) => new Date(b.isoDate) - new Date(a.isoDate)))
+          .then(podcasts => component.setState({ podcasts }));
+          // .then(console.log);
+          // .then((responses) => responses.reduce((acc, { data }) => acc.concat(data.results), []))
+          // .then(podcasts => podcasts.sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate)))
+          // .then((podcasts) => component.setState({ podcasts }))
+          // .catch(console.error);
       })
+
+    // axios('/podcasts.txt')
+    //   .then(({ data }) => data.split('\n'))
+    //   .then(podcastNames => {
+    //     const podcastCalls = podcastNames.map((podcastName) => axios(`${appleAPI}${podcastName.split(' ').join('+')}`));
+    //     // axios('https://itunes.apple.com/search?entity=podcast&term=npr')
+    //     Promise.all(podcastCalls)
+    //       .then((responses) => responses.reduce((acc, { data }) => acc.concat(data.results), []))
+    //       .then(podcasts => podcasts.sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate)))
+    //       .then((podcasts) => component.setState({ podcasts }))
+    //       .catch(console.error);
+    //   })
   }
 
   render() {
     const { podcasts } = this.state;
     const { test } = this;
     const podcastComponents = podcasts.map((podcast) => {
-      return (<PodcastIcon key={podcast.trackId} podcast={podcast} handleClick={test} />);
+      return (<PodcastIcon key={podcast.isoDate} podcast={podcast} handleClick={test} />);
     });
 
     return (
--- src/components/PodcastIcon.js
+++ src/components/PodcastIcon.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 function PodcastIcon({ podcast, handleClick }) {
+  console.log(podcast);
   return (
     <li className="podcast-icon" onClick={() => handleClick(podcast)}>
-      <img src={podcast.artworkUrl600} alt={podcast.artistName} />
+      <img src={podcast.itunes.image} alt={podcast.artistName} />
     </li>
   )
 }
