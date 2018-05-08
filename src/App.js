d098401adab65b6ae6944eb639dc8b59244d251c
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-07 20:29:25 -0500

    update information on podcast episode objects

--- src/App.js
+++ src/App.js
@@ -23,10 +23,6 @@ class App extends Component {
   componentWillMount() {
     let parser = new Parser();
     const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
-
-    // parser.parseURL(CORS_PROXY + )
-    //   .then(console.log);
-
     const component = this;
     const appleAPI = 'https://itunes.apple.com/search?entity=podcast&attribute=titleTerm&term='
     axios('/rss-feeds.txt')
@@ -35,14 +31,11 @@ class App extends Component {
         const rssFeedRequests = rssFeedUrls.map((feedUrl) => parser.parseURL(CORS_PROXY + feedUrl));
         Promise.all(rssFeedRequests)
           .then(responses => responses.map(response => { response.items = response.items.slice(0, 5); return response; }))
-          .then(responses => responses.map(response => { response.items = response.items.slice(0, 5); return response; }))
-          .then(responses => responses.reduce((acc, res) => acc.concat(res.items), []))
+          .then(responses => responses.map(fillOutEpisodesInfo))
+          .then(responses => responses.reduce((acc, res) => acc.concat(res), []))
           .then(podcasts => podcasts.sort((a, b) => new Date(b.isoDate) - new Date(a.isoDate)))
           .then(podcasts => component.setState({ podcasts }));
           // .then(console.log);
-          // .then((responses) => responses.reduce((acc, { data }) => acc.concat(data.results), []))
-          // .then(podcasts => podcasts.sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate)))
-          // .then((podcasts) => component.setState({ podcasts }))
           // .catch(console.error);
       })
 
@@ -77,3 +70,22 @@ class App extends Component {
 }
 
 export default App;
+
+// function limitEpisodes(podcast) {
+//   const episodes = podcast.items.slice();
+
+//   return episodes;
+// }
+
+function fillOutEpisodesInfo(podcast) {
+  const episodes = podcast.items.slice(0);
+  episodes.map(episode => {
+    episode.link = podcast.link
+    episode.title = podcast.title
+    episode.feedUrl = podcast.feedUrl
+    episode.description = podcast.description
+    episode.itunesPodcast = podcast.itunes;
+    return episode;
+  });
+  return episodes;
+}
--- src/components/PodcastIcon.js
+++ src/components/PodcastIcon.js
@@ -4,7 +4,7 @@ function PodcastIcon({ podcast, handleClick }) {
   console.log(podcast);
   return (
     <li className="podcast-icon" onClick={() => handleClick(podcast)}>
-      <img src={podcast.itunes.image} alt={podcast.artistName} />
+      <img src={podcast.itunesPodcast.image} alt={podcast.artistName} />
     </li>
   )
 }
