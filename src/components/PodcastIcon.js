4011bd13929c0b17723cae85b64b1674dbc70753
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-09 23:15:51 -0500

    add podcast list item component

--- src/components/PodcastIcon.js
+++ src/components/PodcastIcon.js
@@ -8,4 +8,17 @@ function PodcastIcon({ podcast, handleClick }) {
   )
 }
 
-export default PodcastIcon;
+const PodcastListItem = ({ podcast, handleClick }) => (
+  <li className="podcast-list-item" onClick={() => handleClick(podcast)}>
+    <img title={podcast.title} src={podcast.itunesPodcast.image} alt={podcast.artistName} />
+    <div className="podcast-info">
+      <b>{ podcast.name }</b>
+      <p>{ podcast.title }</p>
+    </div>
+  </li>
+);
+
+export {
+  PodcastIcon,
+  PodcastListItem
+};
