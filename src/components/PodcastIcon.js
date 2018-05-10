0fa1d0821f6bc7b6eb23f19e628919821f92f281
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-09 23:16:45 -0500

    refactor podcast icon component

--- src/components/PodcastIcon.js
+++ src/components/PodcastIcon.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-function PodcastIcon({ podcast, handleClick }) {
-  return (
-    <li className="podcast-icon" onClick={() => handleClick(podcast)}>
-      <img title={podcast.title} src={podcast.itunesPodcast.image} alt={podcast.artistName} />
-    </li>
-  )
-}
+const PodcastIcon = ({ podcast, handleClick }) => (
+  <li className="podcast-icon" onClick={() => handleClick(podcast)}>
+    <img title={podcast.title} src={podcast.itunesPodcast.image} alt={podcast.artistName} />
+  </li>
+);
 
 const PodcastListItem = ({ podcast, handleClick }) => (
   <li className="podcast-list-item" onClick={() => handleClick(podcast)}>
