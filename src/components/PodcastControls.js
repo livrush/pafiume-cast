2e8781ded27a5b682d00d8bbfd7f032ab23336e0
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-09 22:17:14 -0500

    move podcast control subcomonents into podcast controls file

--- src/components/PodcastControls.js
+++ src/components/PodcastControls.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import PodcastTogglePlay from './podcastControls/PodcastTogglePlay.js';
-import PodcastToggleTime from './podcastControls/PodcastToggleTime.js';
+// import PodcastTogglePlay from './podcastControls/PodcastTogglePlay.js';
+// import PodcastToggleTime from './podcastControls/PodcastToggleTime.js';
 
 const PodcastEpisodeInfo = ({ episode }) => (
   <li className="podcast-control info">
@@ -17,6 +17,22 @@ const PodcastBufferingIcon = ({ buffering }) => buffering ? (
 ) : (
   <li className="podcast-control">
     <i className="fas fa-smile"></i>
+  </li>
+);
+
+const PodcastToggleTime = () => (
+  <li className="podcast-control time" >
+    <i className="fas fa-stopwatch"></i>
+  </li>
+);
+
+const PodcastTogglePlay = ({ togglePlay, playing }) => playing ? (
+  <li className="podcast-control stop" onClick={togglePlay}>
+    <i className="fas fa-stop"></i>
+  </li>
+) : (
+  <li className="podcast-control play" onClick={togglePlay}>
+    <i className="fas fa-play"></i>
   </li>
 );
 
