41fcb6793d8d0d02c0a5bfd197f78eb910bb838c
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-09 22:08:39 -0500

    add podcast episode info component

--- src/components/PodcastControls.js
+++ src/components/PodcastControls.js
@@ -2,21 +2,28 @@ import React from 'react';
 import PodcastTogglePlay from './podcastControls/PodcastTogglePlay.js';
 import PodcastToggleTime from './podcastControls/PodcastToggleTime.js';
 
-const PodcastBufferingIcon = ({ buffering }) => buffering ?
-  (
-    <li className="podcast-control buffering">
-      <i className="fas fa-spinner"></i>
-    </li>
-  ) :
-  (
-    <li className="podcast-control">
-      <i className="fas fa-ok"></i>
-    </li>
-  );
+const PodcastEpisodeInfo = ({ episode }) => (
+  <li className="podcast-control info">
+    {/* <p>{ episode.index }</p> */}
+    <b>{ episode.name }</b>
+    <p>{ episode.title }</p>
+  </li>
+);
 
-const PodcastControls = ({ buffering, playing, togglePlay }) => (
+const PodcastBufferingIcon = ({ buffering }) => buffering ? (
+  <li className="podcast-control buffering">
+    <i className="fas fa-circle-notch"></i>
+  </li>
+) : (
+  <li className="podcast-control">
+    <i className="fas fa-smile"></i>
+  </li>
+);
+
+const PodcastControls = ({ buffering, episode, playing, togglePlay }) => (
   <ul className="podcast-controls">
     <PodcastBufferingIcon buffering={buffering} />
+    <PodcastEpisodeInfo episode={episode} />
     <PodcastToggleTime togglePlay={togglePlay} />
     <PodcastTogglePlay togglePlay={togglePlay} playing={playing} />
   </ul>
