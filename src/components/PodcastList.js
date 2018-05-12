6024f318d7d6131b7322ea339dd67cb38e53d89d
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-11 22:59:54 -0500

    move child components into podcast list file

--- src/components/PodcastList.js
+++ src/components/PodcastList.js
@@ -1,5 +1,21 @@
-import React, { Component } from 'react';
-import { PodcastIcon, PodcastListItem } from './PodcastIcon.js';
+import React from 'react';
+// import { PodcastIcon, PodcastListItem } from './PodcastIcon.js';
+
+const PodcastIcon = ({ podcast, handleClick }) => (
+  <li className="podcast-icon" onClick={() => handleClick(podcast)}>
+    <img title={podcast.title} src={podcast.itunesPodcast.image} alt={podcast.artistName} />
+  </li>
+);
+
+const PodcastListItem = ({ podcast, handleClick, style }) => (
+  <li className="podcast-list-item" onClick={() => handleClick(podcast)} style={style}>
+    <img title={podcast.title} src={podcast.itunesPodcast.image} alt={podcast.artistName} />
+    <div className="podcast-info">
+      <p><b>{ podcast.name }</b></p>
+      <p>{ podcast.title }</p>
+    </div>
+  </li>
+);
 
 const PodcastList = ({ color, podcasts, onClickPodcast }) => {
   const podcastComponents = podcasts.map((podcast, index) => {
